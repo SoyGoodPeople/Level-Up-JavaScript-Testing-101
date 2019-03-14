a1b5bbdede4802ab69531d03303f2f2ff53ea189
Adrian <adrianavila070@gmail.com>
2019-03-14 17:09:29 -0500

    create our first fake funtion

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,6 @@
-import { add, total } from "./App";
+import { total } from "./App";
+
+const add = jest.fn(() => 3);
 
 // Unit test
 // It only test one thing
@@ -6,12 +8,12 @@ test("add", () => {
   // const value = add(1,2);
   // expect(value).toBe(3);
   expect(add(1, 2)).toBe(3);
-  expect(add(5, 2)).toBe(7);
+  expect(add(5, 2)).toBe(3);
 });
 
 
 // Integration test
 // Because it's about test code working together
-test("total", () => {
-  expect(total(5, 20)).toBe('$25');
-});
+// test("total", () => {
+//   expect(total(5, 20)).toBe('$25');
+// });

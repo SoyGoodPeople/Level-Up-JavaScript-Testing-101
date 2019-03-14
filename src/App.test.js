bc9391a222447e9f0e33e2f8550f1ae4d7711c5f
Adrian <adrianavila070@gmail.com>
2019-03-14 16:49:55 -0500

    add integration test for the total function

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,17 @@
-import { add } from './App'
+import { add, total } from "./App";
 
 // Unit test
 // It only test one thing
-test('add', ()=> {
+test("add", () => {
   // const value = add(1,2);
   // expect(value).toBe(3);
-  expect(add(1,2)).toBe(3);
-  expect(add(5,2)).toBe(7);
+  expect(add(1, 2)).toBe(3);
+  expect(add(5, 2)).toBe(7);
+});
+
+
+// Integration test
+// Because it's about code working togeder
+test("total", () => {
+  expect(total(5, 20)).toBe('$25');
 });
